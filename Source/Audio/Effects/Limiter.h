99355d004b081e1a3ad5fcb3f671fd17aea2707a
cianobri <cianobri@gmail.com>
2014-04-26 19:20:33 -0400

    - added limiter

---
+++ Source/Audio/Effects/Limiter.h
@@ -0,0 +1,57 @@
+#if !defined(__Limiter_hdr__)
+#define __Limiter_hdr__
+
+#include "RingBuffer.h"
+#include <Math.h>
+#include <stdio.h>
+//#include "Macros.h"
+#include <algorithm>
+
+#define MAX_DELAY_SAMPLES 5
+
+/*	Limiter
+	----------------
+	Paramaters:	
+		- Attack Time
+		- Release Time
+		- Threshold
+*/
+
+class CLimiter
+{
+public:
+
+	CLimiter(int NumChannels);
+	~CLimiter ();
+
+	// set:
+	void setParam(int parameterID, float value);
+	// get:
+	float getParam(int parameterID);
+
+	void prepareToPlay(float sampleRate);
+
+	void initDefaults();
+
+	// process:
+	void process(float **inputBuffer, int numFrames, bool bypass);
+
+private:
+
+	CRingBuffer<float> **m_ppRingBuffer;
+
+	float m_fAttackInSec;
+	float m_fReleaseInSec;
+	float m_fDelayInSec;
+	float m_fThresh;
+
+	float * m_fPeak;
+	float * m_fCoeff;
+	float * m_fGain;
+
+	float m_fSampleRate;
+	int   m_iNumChannels;
+
+};
+
+#endif
---
+++ Source/Audio/Effects/Limiter.mm
@@ -0,0 +1,157 @@
+//==============================================================================
+//
+//  Limiter.cpp
+//  GestureController
+//
+//  Created by Cian O'Brien on 4/22/14.
+//  Copyright (c) 2014 GTCMT. All rights reserved.
+//
+//==============================================================================
+
+#include "Limiter.h"
+
+CLimiter::CLimiter(int numChannels)
+{
+    
+    m_iNumChannels  =   numChannels;
+	m_fSampleRate   =   0.0f;
+
+	m_fPeak		= new float [numChannels];
+	m_fCoeff	= new float [numChannels];
+	m_fGain		= new float [numChannels];
+
+	m_ppRingBuffer = new CRingBuffer<float> *[numChannels];
+	for (int n = 0; n < numChannels; n++)
+	{
+		m_ppRingBuffer[n]	= new CRingBuffer<float>((int)(MAX_DELAY_SAMPLES));
+		// set indices and buffer contents to zero:
+		m_ppRingBuffer[n]->resetInstance();
+	}
+
+	initDefaults();
+}
+
+CLimiter::~CLimiter()
+{
+    delete [] m_ppRingBuffer;
+	delete [] m_fPeak;
+	delete [] m_fGain;
+	delete [] m_fCoeff;
+}
+
+void CLimiter::initDefaults()
+{  
+	m_fSampleRate	= 0.0f;
+	m_fThresh		= 1.0f;
+	m_fAttackInSec	= 0.01f;
+	m_fReleaseInSec	= 0.5f;
+	m_fDelayInSec   = 0.01f;
+
+	for (int n = 0; n < m_iNumChannels; n++)
+	{
+		m_fPeak[n]		= 0.0f;
+		m_fGain[n]		= 1.0f;
+		m_fCoeff[n]		= 0.0f;
+	}
+}
+
+void CLimiter::prepareToPlay(float sampleRate)
+{
+    m_fSampleRate = sampleRate;
+    
+	// set the write index:
+    for (int n = 0; n < m_iNumChannels; n++)
+    {
+        m_ppRingBuffer[n]->setWriteIdx(m_ppRingBuffer[n]->getReadIdx() + (int)(m_fDelayInSec * m_fSampleRate));
+    }
+}
+
+void CLimiter::setParam(int type, float value)
+{
+	switch(type)
+	{
+		
+		case 0:
+			if (value > 0.0f)
+            {
+				m_fAttackInSec	= value;
+            }
+		break;
+		            
+		case 1:
+			if (value >= 0.0f)
+				m_fReleaseInSec = value;
+		break;
+		      
+		case 2:
+			if (value >= 0.0f)
+				m_fThresh = value;
+		break;
+            
+		default: break;
+		
+	
+	}
+}
+
+void CLimiter::process(float** audioBuffer, int numFrames, bool bypass)
+{
+	if (!bypass)
+    {
+        for (int c = 0; c < m_iNumChannels; c++)
+		{
+			for (int i = 0; i < numFrames; i++)
+			{	
+
+				if (fabs(audioBuffer[c][i]) > m_fPeak[c])
+				{
+					m_fCoeff[c] = m_fAttackInSec;
+				} else {
+					m_fCoeff[c] = m_fReleaseInSec;
+				}
+
+				m_fPeak[c] = (1.0f-m_fCoeff[c]) * m_fPeak[c] + m_fCoeff[c] * fabs(audioBuffer[c][i]);
+
+				if (std::min(1.0f, m_fThresh/m_fPeak[c]) < m_fGain[c])
+				{
+					m_fCoeff[c] = m_fAttackInSec;
+				} else {
+					m_fCoeff[c] = m_fReleaseInSec;
+				}
+
+				m_fGain[c] = (1.0f-m_fCoeff[c]) * m_fGain[c] + m_fCoeff[c] * (std::min(1.0f, m_fThresh/m_fPeak[c]));
+
+				m_ppRingBuffer[c]->putPostInc(audioBuffer[c][i]);
+				audioBuffer[c][i] = m_fGain[c] * m_ppRingBuffer[c]->getPostInc();
+			
+			}
+		}   
+    }
+}
+
+float CLimiter::getParam(int type)
+{
+    switch(type)
+	{
+		
+		case 0:
+            return m_fAttackInSec;
+            break;
+            
+            
+		case 1:
+			return m_fReleaseInSec;
+            break;
+            
+            
+		case 2:
+			return m_fThresh;
+            break;
+            
+		default:
+            break;
+		
+
+	}
+
+}
